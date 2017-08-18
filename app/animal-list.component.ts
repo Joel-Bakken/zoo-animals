08ee046d42ce01bc745be3bd497f5115089b75a0
Joel Bakken <jkbetc@gmail.com>
2017-08-18 14:36:31 -0700

    add the ability to filter animals by under or over two years old

--- app/animal-list.component.ts
+++ app/animal-list.component.ts
@@ -5,10 +5,26 @@ import { Animal } from './animal.model';
   selector: 'animal-list',
   template: `
 
+  <select (change)="onChange($event.target.value)">
+    <option value="allAnimals" selected="selected">All Animals</option>
+    <option value="underTwo">Under 2 Years Old</option>
+    <option value="overTwo">Over 2 Years Old</option>
+  </select>
+
 
 
   <ul class="list-group">
-    <li  *ngFor="let currentAnimal of animals"> <button (click)="editAnimal(currentAnimal)">Edit Animal</button>
+    <li *ngFor="let currentAnimal of childAnimalList | age:filterByAge">
+    <p>Species:</p><h3>{{childSelectedAnimal.species}}</h3>
+    <p>Name:</p><h3>{{childSelectedAnimal.name}}</h3>
+    <p>Age:</p><h3>{{childSelectedAnimal.age}}</h3>
+    <p>Diet:</p><h3>{{childSelectedAnimal.diet}}</h3>
+    <p>Location:</p><h3>{{childSelectedAnimal.location}}</h3>
+    <p>Caretakers:</p><h3>{{childSelectedAnimal.caretakers}}</h3>
+    <p>Sex:</p><h3>{{childSelectedAnimal.sex}}</h3>
+    <p>Likes:</p><h3>{{childSelectedAnimal.likes}}</h3>
+    <p>Dislikes:</p><h3>{{childSelectedAnimal.dislikes}}</h3>
+    <button (click)="editAnimal(currentAnimal)">Edit Animal</button>
     </li>
   </ul>
   `
@@ -18,6 +34,11 @@ export class AnimalListComponent {
   @Input() childAnimalList: Animal[];
   @Output() clickSender = new EventEmitter();
 
+  filterByAge: string = "allAnimals";
+
+  onChange(optionFromMenu) {
+    this.filterByAge = optionFromMenu;
+  }
 
   editButtonHasBeenClicked(animalToEdit: Animal) {
   this.clickSender.emit(animalToEdit);
--- app/edit-animal.component.ts
+++ app/edit-animal.component.ts
@@ -10,18 +10,25 @@ import { Animal } from './animal.model';
     <label>Enter New Name:</label>
     <input [(ngModel)]="childSelectedAnimal.name">
     <p>Age:</p><h3>{{childSelectedAnimal.age}}</h3>
+    <label>Enter New Age:</label>
     <input [(ngModel)]="childSelectedAnimal.age" type="number">
     <p>Diet:</p><h3>{{childSelectedAnimal.diet}}</h3>
+    <label>Enter New Diet:</label>
     <input [(ngModel)]="childSelectedAnimal.diet">
     <p>Location:</p><h3>{{childSelectedAnimal.location}}</h3>
+    <label>Enter New Location:</label>
     <input [(ngModel)]="childSelectedAnimal.location">
     <p>Caretakers:</p><h3>{{childSelectedAnimal.caretakers}}</h3>
+    <label>Enter New Number of Caretakers:</label>
     <input [(ngModel)]="childSelectedAnimal.caretakers" type="number">
     <p>Sex:</p><h3>{{childSelectedAnimal.sex}}</h3>
+    <label>Enter New Animal Sex (Only use to correct error(s) when new animal was entered into the system):</label>
     <input [(ngModel)]="childSelectedAnimal.sex">
     <p>Likes:</p><h3>{{childSelectedAnimal.likes}}</h3>
+    <label>Enter New Likes:</label>
     <input [(ngModel)]="childSelectedAnimal.likes">
     <p>Dislikes:</p><h3>{{childSelectedAnimal.dislikes}}</h3>
+    <label>Enter New Dislikes:</label>
     <input [(ngModel)]="childSelectedAnimal.dislikes">
     <button (click)="doneButtonClicked()">Done</button>
   </div>
